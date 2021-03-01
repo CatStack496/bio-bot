e809ac520485de92e2196b281bfe12ace284b804
Kevin Long <kevin.uu.long@gmail.com>
2021-03-01 17:47:40 -0500

    Fix for deploy
    Also added a bunch of comments!

--- functions/bio/bio.js
+++ functions/bio/bio.js
@@ -4,31 +4,41 @@ const fetch = require('node-fetch');
 const handler = async (event) => {
     try {
         const payload = querystring.parse(event.body);
+        // This gets any parameters received from the command as a String
+        // In this case it should only ever be a name
         const name = payload.text;
+        // We can use this URL to send a contextual response back for < 3 mins
         const resUrl = payload.response_url;
-        let response;
-        try {
-            let parseName = name.toLowerCase().replace(/ /gm, '');
-            response = require(`./bios/${parseName}.json`);
-        }
-
-        catch (error) {
-            console.error(error);
-            response = { text: ":x: That person was not found!" };
-        }
+        // Remove spaces from `name` for use in a URL
+        const parseName = name.replace(/ /gm, '');
+        // A URL could probably be hard-coded here, but we can also get the current URL from the request headers
+        const jsonUrl = `https://${event.headers['x-forwarded-host']}/${parseName}.json`;
+        // Try to find `parseName.json` and set its contents as the value
+        let response = await fetch(jsonUrl, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .catch(error => console.error(error))
+            // If `parseName.json` does not exist (or another error occurs) this is set as the value
+            || { text: ":x: That person was not found!" }
 
         await fetch(resUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
+            // This provides a message preview for notifications since they cannot display blocks and then appends it to the `response`
             body: JSON.stringify(Object.assign({ text: `${name}'s bio!` }, response)),
         })
 
         return {
+            // Slack requires an http 200 response for every request or it will error
             statusCode: 200
         }
     } catch (error) {
+        // Let Slack know if we encountered an error
         return { statusCode: 500, body: error.toString() }
     }
 }
